35a396215f99c0c83438c471adc8115d0815d8ee
Anggie2305 <anggienurprastowo@gmail.com>
2023-12-26 09:55:24 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/firebase_options.dart';
+import 'package:flutter_application_2/pages/splashpage.dart';
+// import 'package:flutter_application_2/pages/AddFormPage.dart';
+import 'package:flutter_application_2/pages/dashboard/DashboardPage.dart';
+// import 'package:flutter_application_2/pages/DetailPage.dart';
+import 'package:flutter_application_2/pages/LoginPage.dart';
+import 'package:flutter_application_2/pages/RegisterPage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MaterialApp(
+    title: 'Lapor Book',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      '/login': (context) => LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      '/dashboard': (context) => const DashboardPage(),
+      // '/add': (context) => AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
