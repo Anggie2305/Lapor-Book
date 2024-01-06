5bd65ce944f8660b833d16864d10b1d276abaa14
Your Name <anggienurprastowo@gmail.com>
2024-01-06 11:53:12 +0700

    Fitur Like

--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,3 @@ Future<void> main() async {
     },
   ));
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(''),
-        ),
-      ),
-    );
-  }
-}
